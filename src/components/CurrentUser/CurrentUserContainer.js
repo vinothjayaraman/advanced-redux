d98f735e1476addce2b8d777455616c5215ced81
Jayaraman <jayaraman@puc.state.oh.us>
2017-11-22 16:05:10 -0500

    react-redux missing changes

---
+++ src/components/CurrentUser/CurrentUserContainer.js
@@ -0,0 +1,20 @@
+import {connect} from 'react-redux';
+import {CurrentUser} from './CurrentUser';
+
+const mapStateToProps = (state)=>{
+    const currentUser = state.get(`currentUser`);
+
+    return {
+        name: currentUser.get(`name`),
+        status: currentUser.get(`status`),
+        id:currentUser.get(`id`)
+    }
+};
+
+const mapDispatchToProps = (dispatch)=>{
+    return {
+        updateStatus:({target:{value}})=>
+    }
+};
+
+export const CurrentUserContainer = connect(mapStateToProps,mapDispatchToProps)(CurrentUser);
